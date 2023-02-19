99c3f9042d21aadc019078ee1634000d2566eca2
ruthvic_s <rsivakumar@ucsd.edu>
2023-02-18 21:57:13 -0800

    Refactored code to fit SRP

--- app/src/main/java/edu/ucsd/cse110/cse110group8_compass/AngleCalculator.java
+++ app/src/main/java/edu/ucsd/cse110/cse110group8_compass/AngleCalculator.java
@@ -1,44 +1,18 @@
 package edu.ucsd.cse110.cse110group8_compass;
 
-
-import android.content.Context;
-import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.util.Pair;
 
 public class AngleCalculator {
-    protected double userLatitude, userLongitude;
-    //private double targetLatitude, targetLongitude;
-    //private Double azimuth;
-    protected final Double NORTH_POLE_LONGITUDE = 135.0000;
-    protected final Double NORTH_POLE_LATITUDE = 90.0000;
-    protected final Location NORTH_POLE_LOCATION;
-
-    private double debugLatSave = 0;
-    private double debugLongSave = 0;
 
     protected Location userLocation;
-    private static AngleCalculator instance;
 
     AngleCalculator (Double userLatitudeM, Double userLongitudeM)    {
-        userLatitude = userLatitudeM;
-        userLongitude = userLongitudeM;
-        //this.azimuth = convertRadian(azimuth);
-        NORTH_POLE_LOCATION = new Location("provider");
-        NORTH_POLE_LOCATION.setLatitude(NORTH_POLE_LATITUDE);
-        NORTH_POLE_LOCATION.setLongitude(NORTH_POLE_LONGITUDE);
+        userLocation = new Location("provider");
+        userLocation.setLatitude(userLatitudeM);
+        userLocation.setLongitude(userLongitudeM);
 
-
-        setUserLocation();
     }
-
-   /* public static AngleCalculator singleton() {
-        if (instance == null) {
-            instance = new AngleCalculator(0.0, 0.0);
-        }
-        return instance;
-    }*/
 
     public double convertToDegree(Float rad) {
         if(rad < 0) {
@@ -49,17 +23,6 @@ public class AngleCalculator {
         }
     }
 
-    protected void setUserLocation() {
-        userLocation = new Location("provider");
-        userLocation.setLatitude(userLatitude);
-        userLocation.setLongitude(userLongitude);
-
-    }
-
-    public float northAngle() {
-        return userLocation.bearingTo(NORTH_POLE_LOCATION);
-    }
-
     private Double calculateBearing(Double target_latitude, Double target_longitude) {
 
         Location targetLocation = new Location(LocationManager.GPS_PROVIDER);
@@ -67,54 +30,27 @@ public class AngleCalculator {
         targetLocation.setLatitude(Double.valueOf(target_latitude));
         targetLocation.setLongitude(Double.valueOf(target_longitude));
 
-
-
         return Double.valueOf(userLocation.bearingTo(targetLocation));
     }
-
 
     public Double angleOnCircle(Double target_latitude, Double target_longitude,  Float azimuthRadian ) {
         Double azimuth = convertToDegree(azimuthRadian);
 
-
         Double targetBearing = calculateBearing( target_latitude, target_longitude);
-
-       // System.out.println(azimuth);
-        //System.out.println(targetBearing);
-
-        //System.out.println("target lat: " + target_longitude);
-
 
         if(targetBearing < 0) {
             targetBearing = 360 + targetBearing;
-
         }
 
         if(azimuth > targetBearing) {
-            System.out.println("case1: " + (360 - azimuth + targetBearing));
-            System.out.println("azimuth: " + azimuth);
-            System.out.println("targetBear"+ targetBearing);
-
             return   360 - azimuth + targetBearing;
-
         }
         else if (azimuth < targetBearing) {
-            if(debugLatSave == target_latitude) {
-                System.out.println("case2: " + (targetBearing - azimuth));
-            }
             return targetBearing - azimuth;
         }
         else {
-            System.out.println("case3:");
             return 0.0;
         }
     }
-
-
-
-
-
-
-
 
 }
--- app/src/main/java/edu/ucsd/cse110/cse110group8_compass/DisplayCircle.java
+++ app/src/main/java/edu/ucsd/cse110/cse110group8_compass/DisplayCircle.java
@@ -11,30 +11,23 @@ import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 
-//DisplayCircile coordiantes all the pins and makes them visible or not
+//DisplayCircle coordinates all the pins and makes them visible or not
 public class  DisplayCircle {
      static private ConstraintLayout circle_constraint;
-     //static private ImageView pin_id;
      static private LiveData<Pair<Double, Double>> userCoordinateLive;
+     //static private Pin[3] pins;
 
-     //static private Pin userPin;
-    // static private Pin northPin;
 
-     //should have pin object of north and user already
-
-     //constructor which takes in the compass constraint
       DisplayCircle (ConstraintLayout circle_m) {
-          circle_constraint = circle_m;
+          this.circle_constraint = circle_m;
      }
 
-     //update the pins so that they are made visible
      public void setUserPin(LiveData<Pair<Double, Double>> userCoordinateLive) {
           this.userCoordinateLive = userCoordinateLive;
      }
 
-     //should take the pin object as input
-     //pin object should have location information
-     public void rotatePin(ImageView targetPinImageView, Pin targetPin, LiveData<Float> targetAzimuth, Activity activity) {
+
+     public void rotatePin(Pin targetPin, LiveData<Float> targetAzimuth, Activity activity) {
            userCoordinateLive.observe((LifecycleOwner) activity, new Observer<Pair<Double, Double>>() {
                 @Override
                 public void onChanged(Pair<Double, Double> doubleDoublePair) {
@@ -43,40 +36,32 @@ public class  DisplayCircle {
                      targetAzimuth.observe((LifecycleOwner) activity, new Observer<Float>() {
                           @Override
                           public void onChanged(Float value) {
-                               //Get the data from the LiveData object here
-
-                               move(targetPinImageView, angleCalculator.angleOnCircle(targetPin.latitude, targetPin.longitude, value).floatValue());
+                               Float pinAngle = angleCalculator.angleOnCircle(targetPin.getLatitude(), targetPin.getLongitude(), value).floatValue();
+                               move(targetPin.getPinImageView(), pinAngle );
 
                           }
                      });
                 }
            });
-           //AngleCalculator angleCalculator = new AngleCalculator(userPin.latitude, userPin.longitude)36
-           //move(targetPinImageView, angleCalculator.angleOnCircle(targetPin.latitude, targetPin.longitude, targetAzimuth).floatValue());
-
      }
 
      //These two need to go into its own Rotator class
-          public void move(ImageView pin_m, Float angle) {
+     public void move(ImageView pin_m, Float angle) {
+          ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) pin_m.getLayoutParams();
+          layoutParams.circleAngle = angle;
+          pin_m.setLayoutParams(layoutParams);
+     }
+
+     public void rotate(ImageView pin_m, long animation_time_m , float start_angle_m, float end_angle_m) {
+          ValueAnimator anim = ValueAnimator.ofFloat(start_angle_m, end_angle_m);
+          anim.addUpdateListener(valueAnimator -> {
+               int val = (Integer) valueAnimator.getAnimatedValue();
                ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) pin_m.getLayoutParams();
-               layoutParams.circleAngle = angle;
+               layoutParams.circleAngle = val;
                pin_m.setLayoutParams(layoutParams);
-          }
-
-          public void rotate2(ImageView pin_m, long animation_time_m , float start_angle_m, float end_angle_m) {
-               ValueAnimator anim = ValueAnimator.ofFloat(start_angle_m, end_angle_m);
-               anim.addUpdateListener(valueAnimator -> {
-                    int val = (Integer) valueAnimator.getAnimatedValue();
-                    ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) pin_m.getLayoutParams();
-                    layoutParams.circleAngle = val;
-                    pin_m.setLayoutParams(layoutParams);
-               });
-               anim.setDuration(animation_time_m);
-               anim.setInterpolator(new LinearInterpolator());
-               anim.start();
-          }
-
-     
-     
-
+          });
+          anim.setDuration(animation_time_m);
+          anim.setInterpolator(new LinearInterpolator());
+          anim.start();
+     }
 }
