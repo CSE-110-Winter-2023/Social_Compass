54ee59d648e100b10c842f157ab52eac8a56c912
HangLiu01 <hangliu2001@gmail.com>
2023-03-08 14:38:06 -0800

    add US3 branch entername

---
+++ app/src/main/java/edu/ucsd/cse110/cse110group8_compass/EnterNameActivity.java
@@ -0,0 +1,21 @@
+package edu.ucsd.cse110.cse110group8_compass;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EnterNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entername);
+    }
+
+}
