84e328ebd940a21733dd9bfc6f81bc537ab6ef9b
ruthvic_s <rsivakumar@ucsd.edu>
2023-03-16 14:41:19 -0700

    finish

--- app/src/main/java/edu/ucsd/cse110/cse110group8_compass/PinBuilder.java
+++ app/src/main/java/edu/ucsd/cse110/cse110group8_compass/PinBuilder.java
@@ -1,9 +1,6 @@
 package edu.ucsd.cse110.cse110group8_compass;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.text.TextUtils;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,9 +13,12 @@ public class PinBuilder {
     String label;
     Double longitude;
     Double latitude;
+
+    String PublicCode;
     Context context;
     ConstraintSet set;
     ConstraintLayout layout;
+
     float density;
 
     public PinBuilder(Context context, ConstraintLayout layout, float density){
@@ -29,6 +29,7 @@ public class PinBuilder {
         this.density = density;
     }
 
+
     public PinBuilder config(){
         this.view.setId(View.generateViewId());
         this.layout.addView(view,0);
@@ -38,26 +39,29 @@ public class PinBuilder {
 
         ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) view.getLayoutParams();
         layoutParams.circleRadius = (int) (20 * density);
+        layoutParams.width = (int) (34 * density);
+        layoutParams.height = (int) (34 * density);
         layoutParams.circleConstraint=R.id.compass;
-        layoutParams.height = 200;
-        layoutParams.width = 100;
-
-        this.view.setBackgroundResource(R.drawable.pindrop);
-        this.view.setLayoutParams(layoutParams);
         this.view.bringToFront();
+        //            android:layout_width="34dp"
+        //            android:layout_height="72dp"
+        //this.view.setBackgroundResource(R.drawable.pindrop);
+        this.view.setLayoutParams(layoutParams);
         return this;
     }
 
     public PinBuilder withLabel(String label){
         this.label = label;
         this.view.setText(label);
-        this.view.setTextSize(14);
-        this.view.setGravity(Gravity.TOP);
-        this.view.setTextColor(Color.BLACK);
-        this.view.setSingleLine(true);
-        this.view.setEllipsize(TextUtils.TruncateAt.END);
         return this;
     }
+
+    public PinBuilder withPublicCode(String PublicCode){
+        this.PublicCode = PublicCode;
+        return this;
+    }
+
+
 
     public PinBuilder withCoordinates(Double longit, Double latit){
         this.longitude = longit;
@@ -67,6 +71,7 @@ public class PinBuilder {
 
     public Pin build(){
         Pin pin = new Pin(label, longitude, latitude);
+        pin.setPublic_code(PublicCode);
         pin.setPinTextView(this.view);
         return pin;
     }
